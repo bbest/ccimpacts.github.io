7c8bb29ce9cbe4f5086aa18cc17c8cd672c6b3c0
knboysen <knboysen@gmail.com>
2016-01-15 10:16:20 -0800

    created linear model

--- sealevel_data.R
+++ sealevel_data.R
@@ -4,9 +4,11 @@
 impacts<- c(1,2,3,4,5,6)
 temp<- c(20,22, 24,28, 28, 32)
 
-real_data<-data.frame(impacts, temp)
-colnames(real_data)<- c("sea_rise", "ave_temp")
+sea_data<-data.frame(impacts, temp)
+colnames(sea_data)<- c("sea_rise", "ave_temp")
 
-View(real_data)
+View(sea_data)
 
-plot(real_data$ave_temp, real_data$sea_rise, ylab = "Sea Level Rise (m)", xlab="Temp")
+model<-lm(sea_rise~ ave_temp, data=real_data)
+plot(sea_data$ave_temp, sea_data$sea_rise, ylab = "Sea Level Rise (m)", xlab="Temp")
+
