908ecb664b2a965bdf46c65eef7c0eb31d70014f
knboysen <knboysen@gmail.com>
2016-01-15 09:33:57 -0800

    changed to data frame

--- sealevel_data.R
+++ sealevel_data.R
@@ -1,9 +1,10 @@
 ##climate change impacts!
 #here is some data
 
-impacts<- c(1,2,3,4,5,6,7)
-temp<- c(20,22, 24,28 ,32)
+impacts<- as.data.frame(c(1,2,3,4,5,6))
+temp<- as.data.frame(c(20,22, 24,28, 28, 32))
 
-real_data<-as.data.frame(merge(impacts, temp))
+real_data<-as.data.frame(c(impacts, temp))
 colnames(real_data)<- c("sea_rise", "ave_temp")
 
+View(real_data)
