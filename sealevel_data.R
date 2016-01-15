e5bdfb2b8c59e836321eea8f2906ffab3fcf18a6
knboysen <knboysen@gmail.com>
2016-01-15 10:13:17 -0800

    edited data frame error

--- sealevel_data.R
+++ sealevel_data.R
@@ -1,12 +1,12 @@
 ##climate change impacts!
 #here is some data
 
-impacts<- as.data.frame(c(1,2,3,4,5,6))
-temp<- as.data.frame(c(20,22, 24,28, 28, 32))
+impacts<- c(1,2,3,4,5,6)
+temp<- c(20,22, 24,28, 28, 32)
 
-real_data<-as.data.frame(c(impacts, temp))
+real_data<-data.frame(impacts, temp)
 colnames(real_data)<- c("sea_rise", "ave_temp")
 
 View(real_data)
 
-plot(real_data$sea_rise, real_data$ave_temp)
+plot(real_data$ave_temp, real_data$sea_rise, ylab = "Sea Level Rise (m)", xlab="Temp")
