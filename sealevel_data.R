eda7f67e23a253d9dc0b5cf69ff3a80f361c1134
knboysen <knboysen@gmail.com>
2016-01-15 10:23:40 -0800

    added model line to plot

--- sealevel_data.R
+++ sealevel_data.R
@@ -1,14 +1,13 @@
 ##climate change impacts!
 #here is some data
 
-impacts<- c(1,2,3,4,5,6)
+impacts<- c(1,1,3,4,5,6)
 temp<- c(20,22, 24,28, 28, 32)
 
 sea_data<-data.frame(impacts, temp)
 colnames(sea_data)<- c("sea_rise", "ave_temp")
 
-View(sea_data)
-
-model<-lm(sea_rise~ ave_temp, data=real_data)
-plot(sea_data$ave_temp, sea_data$sea_rise, ylab = "Sea Level Rise (m)", xlab="Temp")
+model<-lm(sea_rise~ ave_temp, data=sea_data)
+plot(sea_data$ave_temp, sea_data$sea_rise, ylab = "Sea Level Rise (m)", xlab="Temp (C)")
+abline(model, col="red")
 
