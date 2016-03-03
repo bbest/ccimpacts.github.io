71095813ce002e796e84470c8959b9908319312b
Jennifer Kidson <jen.kidson@gmail.com>
2016-03-02 16:17:01 -0800

    Created landing data with interactive slider to select date range.

--- wk08_shiny/app.R
+++ wk08_shiny/app.R
@@ -1,52 +1,50 @@
 #
-# title: "Week 5 ggplot Group Assignment"
+# title: "Week 8 Shiny Assignment"
 # author: "K Boysen, J Kidson, J Palacios-Abrantes"
 # date: "February 29, 2016"
 
 ####### Plot from Week 5 Assignment of octopus landings with a slider to select time range #########
 library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+Octo = read_csv("../Data/OctoLanding.csv")      # Not sure if this section should be inside "server"                                                    # or not.
+test <- Octo[, 1:6] %>% 
+  gather("fishery", "catch", 2:4) %>% 
+  filter(fishery!="Total.Landings") 
 
 # Define UI for application
-ui <- shinyUI(fluidPage(
+ui <- fluidPage(
    # Application title
    titlePanel("Octopus Landings"),
-   # Sidebar with a slider input for number of bins
    sidebarLayout(
       sidebarPanel(
-         sliderInput("range",
-                     "Range:",
-                     min = 1980,
-                     max = 2013,
-                     value = c(1980,2013))
+         sliderInput("range","Date Range:",min = 1980,max = 2013,value = c(1980,2013))
       ),
-
-      # Show a plot of the generated distribution
+      # Show a scatter plot of catch data
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("distPlot"),
+         br(), br(),
+         tableOutput("results")
       )
    )
-))
+)
  
  # Define server logic required to plot octopus landings
  server <- shinyServer(function(input, output) {
- 
-   Octo = read_csv("../Data/OctoLanding.csv")      # Not sure if this section should be inside "server"                                                    # or not.
-   test <- Octo[, 1:6] %>% 
-     gather("fishery", "catch", 2:4) %>% 
-     filter(fishery!="Total.Landings")  
    
-  library(ggplot2)
-  octo_plot <- ggplot(test, aes(x=Year, y=catch)) +
-    geom_point(aes(color=fishery, size=ONI)) +
-    xlim(1980,2013) +                             # I think these will need to be variable names in                                                       # order to let the user change the slider?
-    theme_classic() +
-    ggtitle("Octopus Landings") +
-    labs(x="Year", y="Octopus landings (tons)") +
-    theme(legend.position = "bottom")
-  
-  octo_plot                                       # Not sure if this is necessary but the plot wasn't                                                     # (still isn't) showing up, so I added it
-    })
-
+   output$distPlot <- renderPlot({
+      ggplot(test, aes(x=Year, y=catch)) +
+      geom_point(aes(color=fishery, size=ONI)) +
+      xlim(input$range[1],input$range[2]) +                   
+      theme_classic() +
+      ggtitle("Octopus Landings") +
+      labs(x="Year", y="Octopus landings (tons)") +
+      theme(legend.position = "bottom")
+      })
+ })
+ 
  # Run the application 
 shinyApp(ui = ui, server = server)
 
